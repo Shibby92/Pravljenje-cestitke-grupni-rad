ed0fec4eecc5e4fc5378dfc87b637761854d5f54
Shibby92 <haris_000@hotmail.com>
2014-12-22 18:26:08 +0100

    dodani komentari

--- src/SimpleDrawing.java
+++ src/SimpleDrawing.java
@@ -17,16 +17,16 @@ public class SimpleDrawing extends JPanel implements ActionListener {
 	public int counter2 = 400;
 	public int counter3 = 0;
 	int frame = 0;
-	public int counterSuza=320;
+	public int counterSuza = 320;
 
-	// static Snow snow;
+	 static Snow snow;
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawFrame(g, frame++);
 	}
 
 	public void drawFrame(Graphics g, int frameNumber) {
-
+		// nas snjesko
 		Circle glava = new Circle(650, 300, 55, Color.WHITE);
 		glava.draw(g, frameNumber);
 		Circle trup = new Circle(635, 350, 85, Color.WHITE);
@@ -53,35 +53,35 @@ public class SimpleDrawing extends JPanel implements ActionListener {
 		usta6t.draw(g, frameNumber);
 		Circle usta7t = new Circle(689, 334, 5, Color.BLACK);
 		usta7t.draw(g, frameNumber);
-		Circle suza = new Circle(665,counterSuza, 5, Color.BLACK);
+		Circle suza = new Circle(665, counterSuza, 5, Color.BLACK);
 		suza.draw(g, frameNumber);
 		Snow snijeg = new Snow(Color.WHITE, 0, 0, 800);
+		// benjin snjesko
 		Snowman snjesko = new Snowman(counter2, counter, Color.WHITE);
-		if(counter>400){
+		// pokusaj za nekim cik-cak pokretima
+		if (counter > 400) {
 			counterSuza++;
-			if(counter==500)
-				counterSuza=320;
+			if (counter == 500)
+				counterSuza = 320;
 		}
-			
-			
+
 		counter++;
 		if (counter < 300)
 			counter2++;
 		else
 			counter2--;
-		if (counter>450)
-			counter2+=2;
+		if (counter > 450)
+			counter2 += 2;
 		snjesko.draw(g, frameNumber);
 		snijeg.draw(g, frameNumber);
-		Graphics2D g2d=(Graphics2D)g;
-		Font ft=new Font("Comic Sans",Font.BOLD,40);
+		Graphics2D g2d = (Graphics2D) g;
+		Font ft = new Font("Comic Sans", Font.BOLD, 40);
 		g.setFont(ft);
 		g.setColor(Color.BLACK);
-		if (counter>500)
+		if (counter > 500)
 			g2d.drawString("Usput SRETNA NOVA GODINA!", 70, 540);
 		else
-		g2d.drawString("Bjezi BLESOVE lavina stize!!!", 70, 540);
-		
+			g2d.drawString("Bjezi BLESOVE lavina stize!!!", 70, 540);
 
 	}
 
@@ -113,7 +113,7 @@ public class SimpleDrawing extends JPanel implements ActionListener {
 		window.setResizable(false);
 
 		Timer frameTimer = new Timer(20, drawArea);
-		// snow = new Snow(Color.WHITE, windowHeight);
+//		snow = new Snow(Color.WHITE, windowHeight);
 
 		window.setVisible(true);
 		frameTimer.start();
